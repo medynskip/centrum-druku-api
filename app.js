e892852d7d33f59ad420867ff12f3cc712c5dda4
Piotr Medynski <medynski@outlook.com>
2021-01-04 13:27:38 +0100

    api route corrected

--- app.js
+++ app.js
@@ -1,38 +1,43 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5001;
-const mongoose = require('mongoose');
-var path = require('path')
-const cors = require('cors');
+const mongoose = require("mongoose");
+var path = require("path");
+const cors = require("cors");
 
 //routes import
-const productRouter = require('./routers/products')
-const postsRouter = require('./routers/posts')
-const ordersRouter = require('./routers/orders')
-const usersRouter = require('./routers/users')
+const productRouter = require("./routers/products");
+const postsRouter = require("./routers/posts");
+const ordersRouter = require("./routers/orders");
+const usersRouter = require("./routers/users");
 
-app.use('/public', express.static(path.join(__dirname + '/uploads')));
+app.use("/public", express.static(path.join(__dirname + "/uploads")));
 
 app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 
 //routes assign
-app.use('/api/product', productRouter);
-app.use('/api/blog', postsRouter);
-app.use('/api/order', ordersRouter);
-app.use('/api/user', usersRouter);
-
+app.use("/product", productRouter);
+app.use("/blog", postsRouter);
+app.use("/order", ordersRouter);
+app.use("/user", usersRouter);
 
 //connect to db
-mongoose.connect(process.env.DB_CONNECTION, {
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
-}, () => {
-    console.log('connected to CENTRUM_DRUKU db');
-})
+    useCreateIndex: true,
+  },
+  () => {
+    console.log("connected to CENTRUM_DRUKU db");
+  }
+);
 
-app.listen(port, () => console.log(`Centrum Druku server started on port ${port}`))
+app.listen(port, () =>
+  console.log(`Centrum Druku server started on port ${port}`)
+);
