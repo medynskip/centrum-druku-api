06d00c0bf7156b028102e1f8c20aa5a45751e948
medynskip <medynski@outlook.com>
2021-01-23 15:15:22 +0100

    files routes enhanced for folders handling

--- routers/files.js
+++ routers/files.js
@@ -5,38 +5,34 @@ const fs = require("fs").promises;
 async function getFolders(req, res) {
   // const folder = req.params.path;
   const folder = req.body.currentPath;
-  table = [];
+  let elements = {
+    folders: [],
+    files: []
+  };
   const items = await fs.readdir(`./${folder}`, { withFileTypes: true });
 
   for (const item of items) {
     if (item.isDirectory()) {
-      // await getFolders(`${folder}/${item.name}`);
-      table.push({
+      elements.folders.push({
         name: item.name,
         type: "directory",
         path: folder,
-        parent: req.body.parentPath,
+        // parent: req.body.parentPath,
       });
     } else {
-      table.push({
+      elements.files.push({
         name: item.name,
         type: "file",
         path: folder,
-        parent: req.body.parentPath,
+        // parent: req.body.parentPath,
       });
     }
   }
-  res.send(table);
-  // return table;
+  res.send(elements);
 }
 
 router.post("/show/", function (req, res) {
-  // console.log(req.body);
   getFolders(req, res);
-  // getFolders("./public", req, res);
-  // res.send(getFolders("./public"));
-
-  // res.sendStatus(200);
 });
 
 module.exports = router;
